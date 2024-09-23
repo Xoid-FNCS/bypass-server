aac47cf7df4ae523c6c3ba517ef774dccf6abe7c
XoidFN <162973879+Xoid-FNCS@users.noreply.github.com>
2024-09-23 13:44:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Use CORS middleware
+app.use(cors());
 
 // Serve the main page
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-});
-
-// Proxy requests
-app.use('/proxy', (req, res, next) => {
-    const url = req.query.url;
-    if (!url) {
-        return res.status(400).send('No URL provided');
-    }
-
-    createProxyMiddleware({
-        target: url,
-        changeOrigin: true,
-        onError: (err, req, res) => {
-            console.error('Proxy error:', err);
-            res.status(500).send('Proxy error');
-        }
-    })(req, res, next);
 });
 
 // Start the server
