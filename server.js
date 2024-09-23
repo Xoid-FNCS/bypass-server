56cc42435870d4d51bf444b6958c328f3e5f3666
XoidFN <162973879+Xoid-FNCS@users.noreply.github.com>
2024-09-23 13:46:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Use CORS middleware
 app.use(cors());
+
+// Proxy middleware
+app.use('/proxy', createProxyMiddleware({
+    target: 'http://example.com', // Default target
+    changeOrigin: true,
+    onProxyRes: function (proxyRes) {
+        // Modify response headers to evade blockers
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+        delete proxyRes.headers['X-Frame-Options'];
+        delete proxyRes.headers['Content-Security-Policy'];
+    },
+    onError: (err, req, res) => {
+        console.error('Proxy error:', err);
+        res.status(500).send('Proxy error');
+    }
+}));
 
 // Serve the main page
 app.get('/', (req, res) => {
